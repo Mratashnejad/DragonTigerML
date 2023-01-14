5a3f0f0a0fd4d7f1de0c609052fa6dbdea52a3b7
Mratashnejad <mratashnejad@gmail.com>
2023-01-15 00:30:48 +0400

    check

--- game.py
+++ game.py
@@ -11,10 +11,12 @@ Cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
 Suits = ['Clubs','Diamonds','Hearts','Spades']
 DeckSize = 52 * 8
 c=[]
+counter = 0
 def deck(Cards,Suits):
     for i in range(len(Suits)):
         for j in range(len(Cards)):
             c.append(Cards[j]+" "+Suits[i])
             print(c)
+           
 
 deck(Cards,Suits)
