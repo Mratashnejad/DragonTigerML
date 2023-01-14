6e63d4d2cbc1218d33ded69555fd309ed51c48b6
Mratashnejad <mratashnejad@gmail.com>
2023-01-14 23:58:08 +0400

    Game ruls

---
+++ game.py
@@ -0,0 +1,16 @@
+#how dragon tiger should play.
+
+
+#we have 8 * 52 cards in each deck . 
+#there is a cutting card inside each deck,and that card has no FIX position in the deck.but its almost making deck
+#to the tow part.
+
+
+
+Cards = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+Suits = ['Clubs','Diamonds','Hearts','Spades']
+Decks = Cards * Suits
+
+DeckSize = 52 * 8
+
+print(Decks)
