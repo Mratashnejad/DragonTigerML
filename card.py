ff16a51e5c1f35f47828861b88fe96b0cb90c00c
Alireza Atashnejad <32798129+Mratashnejad@users.noreply.github.com>
2023-01-30 22:40:14 +0400

    update

--- card.py
+++ card.py
@@ -1,61 +1,31 @@
-import collections
-import random
+class Card:
+    suits = ["Spades" , "Heart" , "Clubs" , "Dimonds"]
+    ranks = {'A': 1 , '2':2 , '3':3 , '4':4, '5':5,'6':6, '7':7 , '8':8 , '9':9, '10':10, 'J': 11 , 'Q':12 ,'K':13}
 
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
 
-PlayingCard = collections.namedtuple('card', ['suit' , 'rank'])
+    def __repr__(self):
+        return f"{self.rank} of {self.suit}"
 
-class PlayingDeck :
-    ranks = [str(rank) for rank in range(2,11)] + ['J','Q','K','A']
-    suits = ['Spades' , 'Diamonds' , 'Hearts' , 'Clubs']
-    rank_values = {'J' : 11, 'Q':12,'K':13,'A':1}
-
+class Deck(Card):
     def __init__(self):
-        self._cards = [PlayingCard(rank, suit)
-        for suit in self.suits
-            for rank in self.ranks]
+        self.deck = [Card(suit, rank) for suit in self.suits for rank in self.ranks.keys()]
 
-    def __len__(self):
-        return len(self._cards)
+    def __repr__(self):
+        return str(self.deck)
 
-    def __getitem__(self,position):
-        return self._cards[position]
-
-    def deal(self):
-        return self._cards.pop()
-
-    def play(self):
-        self.Dragon = self.deal()
-        self.Tiger = self.deal()
-
-    def result(self):
-        print("Dragon is :", self.Dragon.rank , "of" , self.Dragon.suit)
-        print("Tiger is :", self.Tiger.rank , "of" , self.Tiger.suit)
-        dragon_value = self.rank_values.get(self.Dragon.rank,self.Dragon.rank)
-        tiger_value = self.rank_values.get(self.Tiger.rank , self.Tiger.rank)
-
-        if dragon_value > tiger_value :
-            print("Dragon won with {} of {}" .format(self.Dragon.rank , self.Dragon.suit))
-        elif dragon_value < tiger_value :
-            print("Tiger won with {} of {}" .format(self.Tiger.rank , self.Tiger.suit))
-        else :
-            print("It's a TIE")
-
+class Shoe(Deck):
+    def __init__(self, number_of_decks):
+        super().__init__()
+        self.number_of_decks = number_of_decks
+        self.shoe = self.deck * number_of_decks
     
+    def __repr__(self):
+        return str(self.shoe)
+
 if __name__ == '__main__':
-    
-    deck = PlayingDeck()
-    random.shuffle(deck._cards)
-    
-    deck.play()
-    deck.result()
-    # for card in deck:
-    #     print(card)
-    
-    # for card in reversed(deck):
-    #     print(card)
-
-    
-
-
-
-
+    # print(Card("Spades", "A"))
+    print(Deck())
+    # print(Shoe(2))
